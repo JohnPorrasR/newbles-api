4dfa64fac5993585ad492ec9004c64d134f6878d
JohnPorrasR <johnporrasr@gmail.com>
2017-10-06 16:06:53 -0500

    actializando

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'password', 'ID_ROL',
+        'username', 'password',
     ];
 
     /**
