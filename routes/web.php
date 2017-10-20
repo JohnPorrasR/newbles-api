49d827b0c06168910d4ab5adb9aa589a1f2c7898
JohnPorrasR <johnporrasr@gmail.com>
2017-10-20 09:57:17 -0500

    Implementando el emvio de email para las alertas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,7 @@ Route::get('/home/tokens', 'HomeController@getTokens')->name('home.getTokens');
 Route::get('/home/clients', 'HomeController@getClients')->name('home.getClients');
 Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('home.getAuthorizedClients');
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('email', function (){
+    Mail::to("johnporrasr@gmail.com")->send(new \App\Mail\Email());
+});
