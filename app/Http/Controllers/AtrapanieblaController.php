ebc5a416bab31c58cf6356b2175118bc9c68f150
JohnPorrasR <johnporrasr@gmail.com>
2017-10-21 15:20:51 -0500

    Actualizando el atrapanieblas y el envio de correos

--- app/Http/Controllers/AtrapanieblaController.php
+++ app/Http/Controllers/AtrapanieblaController.php
@@ -64,7 +64,7 @@ class AtrapanieblaController extends ApiController
         return $this->showOne($res);
     }
 
-    public function update(AtrapanieblasRequest $request, $cod)
+    public function update(Request $request, $cod)
     {
         $ID_ATRAPANIEBLAS = $cod;
         $ID_DISPOSITIVO = $request->input('ID_DISPOSITIVO');
@@ -79,8 +79,9 @@ class AtrapanieblaController extends ApiController
         $FECHA_VIGENCIA = $request->input('FECHA_VIGENCIA');
         $HUMEDAD_PROMEDIO = $request->input('HUMEDAD_PROMEDIO');
         $OBSERVACION = $request->input('OBSERVACION');
-        $USUARIO_MODIFICACION = $request->input('USUARIO_CREACION');
+        $USUARIO_MODIFICACION = $request->input('USUARIO_MODIFICACION');
         $FECHA_MODIFICACION = date("Y-m-d H:i:s");
+        $ESTADO_REGISTRO = 'P';
         $inputs = [
             'ID_ATRAPANIEBLAS' => $ID_ATRAPANIEBLAS,
             'ID_DISPOSITIVO' => $ID_DISPOSITIVO, 'ID_TIPO_TANQUE' => $ID_TIPO_TANQUE,
@@ -89,12 +90,13 @@ class AtrapanieblaController extends ApiController
             'ALTO' => $ALTO, 'LONGITUD' => $LONGITUD, 'LATITUD' => $LATITUD,
             'FECHA_VIGENCIA' => $FECHA_VIGENCIA, 'HUMEDAD_PROMEDIO' => $HUMEDAD_PROMEDIO,
             'OBSERVACION' => $OBSERVACION, 'USUARIO_MODIFICACION' => $USUARIO_MODIFICACION,
-            'FECHA_MODIFICACION' => $FECHA_MODIFICACION
+            'FECHA_MODIFICACION' => $FECHA_MODIFICACION, 'ESTADO_REGISTRO' => $ESTADO_REGISTRO
         ];
 
         $atrapaniebla = $this->atrapanieblaRepo->actualizarAprataniebla($inputs, $ID_ATRAPANIEBLAS);
         if($atrapaniebla->ID_ATRAPANIEBLAS > 0){
-
+            $mensaje = "Se han cambiado los datos del Atrapaniebla, Se necesita su aprovación";
+            $this->emailRepo->envioEmail($mensaje, 1);
         }
         return $this->showOne($atrapaniebla);
     }
